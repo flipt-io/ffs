93a9fd7e7817a8b32e7f95cf943a57619a6543b1
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-03-22 13:44:05 -0400

    feat: move code to examples folder; setup_panic

---
+++ examples/go/basic.go
@@ -0,0 +1,40 @@
+var (
+	store  = &storeMock{}
+	logger = zaptest.NewLogger(t)
+	s      = &Server{
+		logger: logger,
+		store:  store,
+	}
+)
+
+store.On("GetFlag", mock.Anything, mock.Anything, "foo").Return(enabledFlag, nil)
+
+store.On("GetEvaluationRules", mock.Anything, mock.Anything, "foo").Return([]*storage.EvaluationRule{}, nil)
+
+resp, err := s.Evaluate(context.TODO(), &flipt.EvaluationRequest{
+	EntityId: "1",
+	FlagKey:  "foo",
+	Context: map[string]string{
+		"bar": "boz",
+	},
+})
+
+resp, err = s.Evaluate(context.TODO(), &flipt.EvaluationRequest{
+	EntityId: "1",
+	FlagKey:  "bar",
+	Context: map[string]string{
+		"bar": "boz",
+	},
+})
+
+resp, err = s.Evaluate(context.TODO(), &flipt.EvaluationRequest{
+	EntityId: "1",
+	FlagKey:  "boz",
+	Context: map[string]string{
+		"bar": "boz",
+	},
+})
+
+flag, err := s.GetFlag(context.TODO(), &flipt.GetFlagRequest{
+	Key: "foo",
+})
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,30 @@
 use std::fs;
-
+use human_panic::setup_panic;
 use tree_sitter::{Parser, Query, QueryCursor};
 
 fn main() {
-    let code = r#"
-	var (
-		store  = &storeMock{}
-		logger = zaptest.NewLogger(t)
-		s      = &Server{
-			logger: logger,
-			store:  store,
-		})
+    setup_panic!();
 
-	store.On("GetFlag", mock.Anything, mock.Anything, "foo").Return(enabledFlag, nil)
+    let code = fs::read_to_string("./examples/go/basic.go").expect("Unable to read file");
 
-	store.On("GetEvaluationRules", mock.Anything, mock.Anything, "foo").Return([]*storage.EvaluationRule{}, nil)
-
-	resp, err := s.Evaluate(context.TODO(), &flipt.EvaluationRequest{
-		EntityId: "1",
-		FlagKey:  "foo",
-		Context: map[string]string{
-			"bar": "boz",
-		},
-	})
-
-	require.NoError(t, err)
-	assert.False(t, resp.Match)
-	assert.Equal(t, flipt.EvaluationReason_UNKNOWN_EVALUATION_REASON, resp.Reason)
-    "#;
     let mut parser = Parser::new();
+
     let go_lang = tree_sitter_go::language();
     parser
         .set_language(go_lang)
         .expect("Error loading Go grammar");
 
-    let parsed = parser.parse(code, None).unwrap();
+    let parsed = parser.parse(&code, None).expect("Error parsing code");
 
     let rules = fs::read_to_string("./rules/go.txt").expect("Unable to read file");
 
-    let query = Query::new(go_lang, &rules).unwrap();
-
+    let query = Query::new(go_lang, &rules).expect("Error loading query");
     let mut query_cursor = QueryCursor::new();
     let all_matches = query_cursor.matches(&query, parsed.root_node(), code.as_bytes());
     let flag_key_idx = query.capture_index_for_name("v").unwrap();
+
     for each_match in all_matches {
         // iterate over all captures called "raise"
-        // ignore captures such as "fn-name"
         for capture in each_match
             .captures
             .iter()
