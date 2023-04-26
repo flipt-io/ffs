ce67125013919d54b36d0cea4b9899d8bc8f5b28
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-04-25 21:23:09 -0400

    chore: extract to lib

---
+++ src/types/args.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+use super::language::SupportedLanguage;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long, value_enum)]
+    pub language: SupportedLanguage,
+    #[arg(short, long, help = "Path to output file (default STDOUT)")]
+    pub output: Option<String>,
+    #[arg(short, long, help = "Path to directory to scan (default .)")]
+    pub directory: Option<String>,
+}
