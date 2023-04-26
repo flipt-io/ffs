6a1317bb5a1c26d930b766e46777a2fe1764e0be
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-04-26 15:57:57 -0400

    feat: basic querying of Flipt server with flag keys (#3)
    * feat: basic querying of Flipt server with flag keys
    
    * chore: get rid of writer, just make as part of main/binary for now
    
    * chore: get rid of querier for now

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,59 @@
 use anyhow::{Ok, Result};
 use clap::Parser;
 use human_panic::setup_panic;
+use types::token::{Token, TokenSet};
 
-use crate::{
-    ffs::{scanner::Scanner, writer::Writer},
-    types::args::Args,
-};
+use crate::{ffs::scanner::Scanner, types::args::Args};
 mod ffs;
 mod types;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     setup_panic!();
 
     let args = Args::parse();
 
     let mut ffs = Scanner::new(args.language, args.dir);
     let tokens = ffs.scan()?;
-    let writer = Writer::new(args.output);
 
-    writer.write(tokens)?;
+    flipt::meta::MetaClient::new(flipt::Config::default())?
+        .info()
+        .get()
+        .await?;
+
+    let flipt_client = flipt::api::ApiClient::new(flipt::Config::default())?;
+
+    for k in tokens.keys() {
+        flipt_client
+            .flags()
+            .get(&flipt::api::flag::FlagGetRequest {
+                namespace_key: None,
+                key: k.to_string(),
+            })
+            .await?;
+    }
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn write_output(tokens: &TokenSet, to: Option<String>) -> Result<()> {
+    let mut out_writer: Box<dyn std::io::Write> = match to {
+        Some(s) => Box::new(std::fs::File::create(s)?),
+        None => Box::new(std::io::stdout()),
+    };
+
+    for (k, v) in tokens {
+        for loc in v {
+            let t = Token {
+                key: k.to_string(),
+                loc: loc.clone(),
+            };
+
+            let json = serde_json::to_string(&t)?;
+            writeln!(out_writer, "{json}")?;
+        }
+    }
 
     Ok(())
 }
