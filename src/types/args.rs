1361c82e4be0ddc54b6a0e01e8dcc67f01fec43d
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-04-28 12:00:52 -0400

    feat: Query for missing flags (#6)
    * feat: redo query logic for missing flags
    
    * feat: handle duplicate keys

--- src/types/args.rs
+++ src/types/args.rs
@@ -7,8 +7,10 @@ use super::language::SupportedLanguage;
 pub struct Args {
     #[arg(short, long, value_enum)]
     pub language: SupportedLanguage,
-    #[arg(short, long, help = "Path to output file (default STDOUT)")]
+    #[arg(short, long, help = "Path to output file (default: STDOUT)")]
     pub output: Option<String>,
-    #[arg(short, long, help = "Path to directory to scan (default .)")]
+    #[arg(short, long, help = "Path to directory to scan (default: .)")]
     pub dir: Option<String>,
+    #[arg(short, long, help = "Namespace to scan (default: 'default')")]
+    pub namespace: Option<String>,
 }
