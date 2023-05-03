0b5ee46566f0058012922e8522380286b4044e19
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-05-03 15:35:16 -0400

    feat: reformat errors payload

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,32 @@ use std::process::ExitCode;
 use anyhow::Result;
 use clap::Parser;
 use human_panic::setup_panic;
+use serde::Serialize;
 
 use crate::{
     ffs::scanner::Scanner,
-    types::{
-        flag::Flag,
-        args::Args,
-    },
+    types::{flag::Flag, language::SupportedLanguage},
 };
 
 use futures::future::join_all;
 
 mod ffs;
 mod types;
+
+#[derive(Parser, Clone, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long, value_enum)]
+    pub language: SupportedLanguage,
+    #[arg(short, long, help = "Path to output file (default: STDOUT)")]
+    pub output: Option<String>,
+    #[arg(short, long, help = "Path to directory to scan (default: .)")]
+    pub dir: Option<String>,
+    #[arg(short, long, help = "Namespace to scan (default: 'default')")]
+    pub namespace: Option<String>,
+}
+
+const NOT_FOUND_ERROR_CODE: i32 = 5;
 
 #[tokio::main]
 async fn main() -> Result<ExitCode> {
@@ -37,42 +50,69 @@ async fn main() -> Result<ExitCode> {
 
     let flipt_client = &flipt::api::ApiClient::new(flipt_config)?;
 
-    let flag_results : Vec<Flag> = join_all(found_flags
-        .into_iter()
-        .map(|f| async move {
-            let resp = flipt_client.flags().get(&flipt::api::flag::FlagGetRequest{
+    let flag_results: Vec<Flag> = join_all(found_flags.into_iter().map(|f| async move {
+        let resp = flipt_client
+            .flags()
+            .get(&flipt::api::flag::FlagGetRequest {
                 namespace_key: Some(f.namespace_key.clone()),
                 key: f.flag_key.clone(),
-            }).await;
+            })
+            .await;
 
-            match resp {
-                Ok(_) => None,
-                Err(error) => {
-                    match error.downcast_ref::<flipt::error::Error>() {
-                        Some(flipt::error::Error::Upstream(e)) => {
-                            if e.code == 5 {
-                                Some(f)
-                            } else {
-                                None
-                            }
-                        },
-                        _ => None,
+        match resp {
+            Ok(_) => None,
+            Err(error) => match error.downcast_ref::<flipt::error::Error>() {
+                Some(flipt::error::Error::Upstream(e)) => {
+                    if e.code == NOT_FOUND_ERROR_CODE {
+                        Some(f)
+                    } else {
+                        None
                     }
-                },
-            }
-        })).await.into_iter().flatten().collect();
+                }
+                _ => None,
+            },
+        }
+    }))
+    .await
+    .into_iter()
+    .flatten()
+    .collect();
 
     let mut out_writer: Box<dyn std::io::Write> = match args.output {
         Some(s) => Box::new(std::fs::File::create(s)?),
         None => Box::new(std::io::stdout()),
     };
 
-
     // ensure all found flags exist in flipt, write to output if not
     if !flag_results.is_empty() {
-        let json = serde_json::to_string(&flag_results)?;
+        let results = Results {
+            errors: flag_results
+                .into_iter()
+                .map(|f| Error {
+                    message: format!(
+                        "Flag: [key: {}, namespace: {}] not found in your Flipt instance",
+                        f.flag_key, f.namespace_key
+                    ),
+                    location: f.location,
+                })
+                .collect(),
+        };
+        let json = serde_json::to_string(&results)?;
         writeln!(out_writer, "{json}")?;
     }
 
     Ok(ExitCode::SUCCESS)
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Results {
+    errors: Vec<Error>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Error {
+    message: String,
+    location: crate::types::flag::Location,
 }
--- src/types/args.rs
+++ src/types/args.rs
@@ -1,16 +1,1 @@
-use clap::Parser;
 
-use super::language::SupportedLanguage;
-
-#[derive(Parser, Clone, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
-    #[arg(short, long, value_enum)]
-    pub language: SupportedLanguage,
-    #[arg(short, long, help = "Path to output file (default: STDOUT)")]
-    pub output: Option<String>,
-    #[arg(short, long, help = "Path to directory to scan (default: .)")]
-    pub dir: Option<String>,
-    #[arg(short, long, help = "Namespace to scan (default: 'default')")]
-    pub namespace: Option<String>,
-}
