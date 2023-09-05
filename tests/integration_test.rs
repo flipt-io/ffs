8add46dc9f1148569210e4881585ea6668a9ec62
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-09-05 12:18:50 -0400

    chore: setup dependabot + tests (#49)
    * chore: configure dependabot
    
    * chore: add tests; refactor lib
    
    * chore: Update README with brew instructions

---
+++ tests/integration_test.rs
@@ -0,0 +1,12 @@
+use ffs::scanner::Scanner;
+use ffs::types::language::SupportedLanguage;
+
+#[test]
+fn test_scan_go() {
+    let results = Scanner::new(SupportedLanguage::Go, Some("examples/go".to_string()))
+        .scan()
+        .unwrap();
+
+    assert_eq!(results.len(), 6);
+    assert!(results.iter().any(|f| f.key == "foo"));
+}
